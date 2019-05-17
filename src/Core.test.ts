fcbccf43cf1158909e064e86e35efaf795eec2c3
Nils Ceberg <nils.ceberg@gmail.com>
2019-05-18 00:45:55 +0200

    Update character parser to work with Maybe

--- src/Core.test.ts
+++ src/Core.test.ts
@@ -2,11 +2,11 @@ import { Parser, Character } from "./Core";
 import { StringSource, SourcePointer, char } from "./Source";
 
 test("character parser", () => {
-//	const source = new StringSource("hello");
-//	const ptr = new SourcePointer(source);
-//
-//	const [result, rest] = Character.parse(ptr);
-//
-//	expect(result).toStrictEqual("h");
-//	expect(rest.equals("ello")).toBeTruthy();
+	const source = new StringSource("hello");
+	const ptr = new SourcePointer(source);
+
+	const [result, rest] = Character.parse(ptr).from();
+
+	expect(result).toStrictEqual("h");
+	expect(rest.equals("ello")).toBeTruthy();
 });
