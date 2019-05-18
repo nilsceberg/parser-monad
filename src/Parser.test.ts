03d842ebcd44f1d0f8873991d8175aafd190c235
Nils Ceberg <nils.ceberg@gmail.com>
2019-05-18 02:06:41 +0200

    Implement more character parsers

--- src/Parser.test.ts
+++ src/Parser.test.ts
@@ -1,7 +1,5 @@
 import { StringSource, SourcePointer } from "./Source";
-import { Lit } from "./Parser";
-import { Character } from "./Core";
-
+import { Lit, Alphanumeric } from "./Parser";
 
 test("lit parser", () => {
 	const source = new StringSource("hello");
@@ -12,4 +10,23 @@ test("lit parser", () => {
 	const [result, rest] = Lit("h").parse(ptr).from();
 	expect(result).toStrictEqual("h");
 	expect(rest.equals("ello")).toBeTruthy();
+});
+
+test("alphanumeric parser", () => {
+	const s1 = new StringSource("hello");
+	const p1 = new SourcePointer(s1);
+
+	const s2 = new StringSource("3ello");
+	const p2 = new SourcePointer(s2);
+
+	const s3 = new StringSource("?ello");
+	const p3 = new SourcePointer(s3);
+
+	let [result] = Alphanumeric.parse(p1).from();
+	expect(result).toStrictEqual("h");
+
+	[result] = Alphanumeric.parse(p2).from();
+	expect(result).toStrictEqual("3");
+
+	expect(Alphanumeric.parse(p3).isJust()).toBeFalsy();
 });
--- src/Parser.ts
+++ src/Parser.ts
@@ -1,4 +1,4 @@
-import { Character } from "./Core";
+import { Character, Fail } from "./Core";
 import { char } from "./Source";
 
 // Derived
@@ -7,3 +7,8 @@ export const Lit = (c: char) => Character.matches(x => x === c);
 // Trivial
 export const Space = Character.matches(x => " \t\n".includes(x));
 export const Digit = Character.matches(x => "0123456789".includes(x));
+export const Letter = Character.matches(x => {
+	const a = x.charCodeAt(0);
+	return (a > 0x40 && a < 0x5B) || (a > 0x60 && a < 0x7B);
+});
+export const Alphanumeric = Letter.or(Digit);
